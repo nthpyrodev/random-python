611ea33ede06d8b40447cd8d147589f36e19d2e1
nthpyrodev <hi@nthpyro.dev>
2025-04-16 12:21:40 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -9,6 +9,7 @@ def hangman():
     word = random.choice(words)
     max_guesses = len(word) + 5
     seperated = list(word)
+    guesses = []
     display = ["_" for _ in seperated]
     incorrect_guesses = 0
 
@@ -16,21 +17,27 @@ def hangman():
         print(" ".join(display))
         guess = input("Guess a letter: ").lower()
 
-        if guess in seperated:
-            for index, letter in enumerate(seperated):
-                if letter == guess:
-                    display[index] = guess
+        if len(guess) <= 1 and not any(char.isdigit() for char in guess):
+            if guess not in guesses:
+                guesses.append(guess)
+                if guess in seperated:
+                    for index, letter in enumerate(seperated):
+                        if letter == guess:
+                            display[index] = guess
+                else:
+                    incorrect_guesses += 1
+                    print(f"Incorrect guess! You have {max_guesses - incorrect_guesses} guesses left.")
+            else:
+                print("Already tried")
+            if "_" not in display:
+                print("Congrats! You guessed the word:", word)
+                break
         else:
-            incorrect_guesses += 1
-            print(f"Incorrect guess! You have {max_guesses - incorrect_guesses} guesses left.")
-
-        if "_" not in display:
-            print("Congrats! You guessed the word:", word)
-            break
+            print("Invalid Input")
     else:
         print("You lost because you ran out of guesses. The word was:", word)
 
-if play == "y" or "yes":
+if play == "y" or play == "yes":
     hangman()
 else:
     print("If you didn't want to play, why on earth did you run me?")
